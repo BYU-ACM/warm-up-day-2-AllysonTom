85b4b196c0f11b6ba0a6e18925b4a0d1aafc758c
Allyson Tom <goforth@Allysons-MBP-5.app.byu.edu>
2017-01-12 12:53:55 -0700

    bisection/binary search

--- sol.py
+++ sol.py
@@ -7,7 +7,15 @@ def Binary_Search(arr, to_find):
      returns: index (int), x, s.t. arr[x] == to_find
               None(none-type) if for all x, arr[x] != to_find
   """
-  pass
+  L, R = 0, len(arr)-1
+  while L <= R:
+    m = (L+R)/2
+    if arr[m] < to_find:
+      L = m+1
+    elif arr[m] > to_find:
+      R = m-1
+    else:
+      return m
 
 def Bisection(func, left_side, right_side, tol=1e-5):
   """A direct implementation of Newton's Method
@@ -20,4 +28,21 @@ def Bisection(func, left_side, right_side, tol=1e-5):
      returns: root (float), x, s.t. abs(func(x))<tol
               None(none-type) if func(left_side), func(right_side) < 0 or func(left_side), func(right_side) > 0
   """
-  pass
+  def sign(num):
+    if num < 0:
+      return False
+    else:
+      return True
+
+  N = 1
+  while N < 100000:
+    c = (left_side+right_side)/2.
+    print c
+    if func(c) == 0 or abs((left_side-right_side)/2.) <= tol:
+      return c
+    N += 1
+    if sign(func(c)) == sign(func(left_side)):
+      left_side = c
+    else:
+      right_side = c
+  return "Reached maximum number of iterations"
